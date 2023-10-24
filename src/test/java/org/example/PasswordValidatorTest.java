26bd942f4e449ef0dc1c616c913690b234a08492
최미래 <miraexhoi@gmail.com>
2023-10-24 18:18:53 +0900

    Changes

--- src/test/java/org/example/PasswordValidatorTest.java
+++ src/test/java/org/example/PasswordValidatorTest.java
@@ -13,4 +13,12 @@ public class PasswordValidatorTest {
                 .doesNotThrowAnyException();
 
     }
+
+    @DisplayName("비밀번호가 최소 8자 이하거나, 12자 이상일 경우 IllegalArgumentException 예외 발생")
+    @Test
+    void validatePasswordTest2() {
+        assertThatCode(() -> PasswordValidator.validate("aadd"))
+                .isInstanceOf(IllegalAccessError.class)
+                .hasMessage("비밀번호는 최소 8자 이상 12자 이하여야 한다.");
+    }
 }
