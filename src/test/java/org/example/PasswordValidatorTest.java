56608062a5ef9855cb15deefe751fd0d92044baf
최미래 <miraexhoi@gmail.com>
2023-10-24 18:25:29 +0900

    Changes

--- src/test/java/org/example/PasswordValidatorTest.java
+++ src/test/java/org/example/PasswordValidatorTest.java
@@ -2,6 +2,8 @@ package org.example;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 
@@ -15,9 +17,10 @@ public class PasswordValidatorTest {
     }
 
     @DisplayName("비밀번호가 최소 8자 이하거나, 12자 이상일 경우 IllegalArgumentException 예외 발생")
-    @Test
-    void validatePasswordTest2() {
-        assertThatCode(() -> PasswordValidator.validate("aadd"))
+    @ParameterizedTest
+    @ValueSource(strings = {"aabb","aabbccddeeffgg"})
+    void validatePasswordTest2(String password) {
+        assertThatCode(() -> PasswordValidator.validate(password))
                 .isInstanceOf(IllegalAccessError.class)
                 .hasMessage("비밀번호는 최소 8자 이상 12자 이하여야 한다.");
     }
