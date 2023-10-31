0ccf98ae0913194fc1fb8020b24664102f6fd34e
최미래 <miraexhoi@gmail.com>
2023-10-31 17:47:10 +0900

    fix error

--- src/test/java/org/example/UserTest.java
+++ src/test/java/org/example/UserTest.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
+    @DisplayName("패스워드 초기화")
+    @Test
+    void passwordTest() {
+        // given
+        User user = new User();
 
+        // when
+        user.initPassword();
+
+        //then
+        assertThat(user.getPassword()).isNotNull();
+    }
 }
