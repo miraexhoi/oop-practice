926ecc0f4bca26aa5c7e72435f0c6bf4ceed9ef1
최미래 <miraexhoi@gmail.com>
2023-11-01 19:37:57 +0900

    Changes

--- src/test/java/org/example/WrongFixedPasswordGenerator.java
+++ src/test/java/org/example/WrongFixedPasswordGenerator.java
@@ -3,6 +3,6 @@ package org.example;
 public class WrongFixedPasswordGenerator implements PasswordGenerator{
     @Override
     public String generatePassword() {
-        return "abcdefgh"; // 8글자
+        return "ab"; // 2글자
     }
 }
