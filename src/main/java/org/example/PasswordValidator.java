ff2ddb8208aa3e1931b15aee944aa284e8241ec8
최미래 <miraexhoi@gmail.com>
2024-01-09 21:52:58 +0900

    Refactor :: code refactoring + add comments

--- src/main/java/org/example/PasswordValidator.java
+++ src/main/java/org/example/PasswordValidator.java
@@ -2,12 +2,23 @@ package org.example;
 
 public class PasswordValidator {
 
-    private static final String WRONG_PASSWORD_EXCEPTION_MESSAGE = "비밀번호는 최소 8자 이상 12자 이하여야 한다.";
-
-    public static void validate(String password) {
+    /**
+     * 비밀번호가 8자 미만 또는 12자 초과하는 경우 IllegalArgumentException 예외가 발생한다.
+     */
+    public void validate(String password) {
         int length = password.length();
-        if(length < 8 || length >12) {
-            throw new IllegalArgumentException(WRONG_PASSWORD_EXCEPTION_MESSAGE);
+
+        if (length < 8 || length > 12) {
+            throw new IllegalArgumentException("비밀번호는 최소 8자 이상 12자 이하여야 한다.");
+        }
+    }
+
+    public void validate2(PasswordGeneratePolicy passwordGeneratePolicy) {
+        String password = passwordGeneratePolicy.generatePassword();
+
+        int length = password.length();
+        if (length < 8 || length > 12) {
+            throw new IllegalArgumentException("비밀번호는 최소 8자 이상 12자 이하여야 한다.");
         }
     }
 }
--- src/test/java/org/example/PasswordValidatorTest.java
+++ src/test/java/org/example/PasswordValidatorTest.java
@@ -5,23 +5,64 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.assertj.core.api.Assertions.*;
 
 public class PasswordValidatorTest {
-    @DisplayName("비밀번호가 최소 8자 이상, 12자 이하면 예외 발생 x")
+
+    /**
+     * 비밀번호는 최소 8자 이상 12자 이하여야 한다.
+     * 비밀번호가 8자 미만 또는 12자 초과인 경우 IllegalArgumentException 예외를 발생시킨다.
+     * 경계조건에 대해 테스트 코드를 작성해야 한다.
+     */
+    @DisplayName("비밀번호가 최소 8자 이상, 12자 이하면 예외가 발생하지 않는다.")
     @Test
     void validatePasswordTest() {
-        assertThatCode(() -> PasswordValidator.validate("serverwizard"))
-                .doesNotThrowAnyException();
+        // given
+        String password = "serverwizard";
+        PasswordValidator passwordValidator = new PasswordValidator();
 
+        // when, then
+        assertThatCode(() -> passwordValidator.validate(password))
+                .doesNotThrowAnyException();
     }
 
-    @DisplayName("비밀번호가 최소 8자 이하거나, 12자 이상일 경우 IllegalArgumentException 예외 발생")
+    @DisplayName("비밀번호가 8자 미만 또는 12자 초과하는 경우 IllegalArgumentException 예외가 발생한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"aabb","aabbccddeeffgg"})
-    void validatePasswordTest2(String password) {
-        assertThatCode(() -> PasswordValidator.validate(password))
-                .isInstanceOf(IllegalAccessError.class)
+    @ValueSource(strings = {"aabbcce", "aabbccddeeffg"})
+    void validatePasswordTest2(String value) {
+        // given
+        PasswordValidator passwordValidator = new PasswordValidator();
+
+        // when, then
+        assertThatCode(() -> passwordValidator.validate(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("비밀번호는 최소 8자 이상 12자 이하여야 한다.");
+    }
+
+    /**
+     * 테스트 하기 쉬운 코드를 작성하다 보면 더 낮은 결합도를 가진 설계를 얻을 수 있다.
+     */
+    @DisplayName("비밀번호가 최소 8자 이상, 12자 이하면 예외가 발생하지 않는다.")
+    @Test
+    void validatePasswordTest2() {
+        // given
+        PasswordValidator passwordValidator = new PasswordValidator();
+
+        // when, then
+        assertThatCode(() -> passwordValidator.validate2(new CorrectPasswordGenerator()))
+                .doesNotThrowAnyException();
+    }
+
+
+    @DisplayName("비밀번호가 8자 미만 또는 12자 초과하는 경우 IllegalArgumentException 예외가 발생한다.")
+    @Test
+    void validatePasswordTest3() {
+        // given
+        PasswordValidator passwordValidator = new PasswordValidator();
+
+        // when, then
+        assertThatCode(() -> passwordValidator.validate2(new WrongPasswordGenerator()))
+                .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("비밀번호는 최소 8자 이상 12자 이하여야 한다.");
     }
 }
