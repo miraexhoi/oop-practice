e202935a81a7095938c3f84e2cdada4260ce7553
최미래 <miraexhoi@gmail.com>
2024-01-14 22:51:03 +0900

    Refactor :: Code Refactoring

--- src/main/java/org/example/calculator/Calculator.java
+++ src/main/java/org/example/calculator/Calculator.java
@@ -1,7 +1,14 @@
 package org.example.calculator;
 
+import java.util.List;
+
 public class Calculator {
+    private static final List<NewArithmeticOperator> arithmeticOperators = List.of(new AdditionOperator(), new SubstractionOperator(), new DivisionOperator(), new MultiplicationOperator());
     public static int calculate(int operand1, String operator, int operand2) {
-        return ArithmeticOperator.calculate(operand1, operator, operand2);
+        return arithmeticOperators.stream()
+                .filter(arithmeticOperator -> arithmeticOperator.supports(operator))
+                .map(arithmeticOperators -> arithmeticOperators.calculate(operand1, operand2))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("올바은 사칙연산이 아닙니다."));
     }
 }
